0fdf3fd6d4da4a6d8e643d36879923ab51114da4
sravani bikkina <140046448+SravaniBikkina@users.noreply.github.com>
2023-09-29 10:57:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
-from flask import Flask, request
-import hashlib
+from flask import Flask
+import socket
 
 app = Flask(__name__)
-
-def generate_unique_id():
-    ip_address = request.remote_addr  # Get user's IP address
-    unique_id = hashlib.md5(ip_address.encode()).hexdigest()  # Generate unique ID using IP address
-    return unique_id
 
 @app.route('/')
 @app.route('/hello')
 @app.route('/hello/')
 def hello_world():
-    host_id = generate_unique_id()  # Generate a unique host ID based on user's IP address
-    return 'Hello World! Host ID: {}\n'.format(host_id)
+    return 'Hello World!\n'
 
 @app.route('/hello/<username>')
 def hello_user(username):
-    host_id = generate_unique_id()  # Generate a unique host ID based on user's IP address
-    return 'Why Hello %s! Host ID: %s\n' % (username, host_id)
+    return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' port=5000)
+    # Get the local machine's IP address
+    host_ip = socket.gethostbyname(socket.gethostname())
+    print("Server IP Address:", host_ip)
 
+    # Run the Flask app with the dynamically generated IP address
+    app.run(host=host_ip, port=5000)
